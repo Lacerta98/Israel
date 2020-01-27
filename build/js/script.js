0533731236ab94222b2cdb7faf4b3e166468b77b
ap.ilicheva <ap.ilicheva@tensor.ru>
2020-01-27 04:25:21 +0300

    Доделала переключение отзывов, подвал и диалоговое окно + мелкие правки

--- build/js/script.js
+++ build/js/script.js
@@ -1,8 +1,23 @@
 "use strict";
+let reviewsLength = null;
+let reviewCountText = null;
+let reviewList = null;
 
 window.onload = function() {
+  const reviewsTextLength = document.getElementsByClassName('review-length')[0];
   const askButtons = Array.from(document.getElementsByClassName('ask-list-button'));
+  const sliderButtonBack = document.getElementsByClassName('review-back')[0];
+  const sliderButtonForward = document.getElementsByClassName('review-forward')[0];
+
+  reviewsLength = document.getElementsByClassName('review-container').length;
+  reviewCountText = document.getElementsByClassName('item')[0];
+  reviewList = document.getElementsByClassName('review-container');
+
+
+  reviewsTextLength.textContent = reviewsLength;
   askButtons.forEach(askButton => askButton.onclick = showAsk);
+  sliderButtonBack.onclick = clickSliderBack;
+  sliderButtonForward.onclick = clickSliderForward;
 };
 
 const showAsk = function (event) {
@@ -10,4 +25,24 @@ const showAsk = function (event) {
   activeButton.classList.toggle('ask-list-button-close');
   activeButton.classList.toggle('ask-list-button-show');
   activeButton.nextSibling.nextSibling.classList.toggle('hidden');
+};
+
+const clickSliderBack = function (event) {
+  let reviewCount = Number(reviewCountText.textContent);
+  if (reviewCount !== 1) {
+    reviewCount = reviewCount - 1;
+    reviewList[reviewCount - 1].classList.remove('visually-hidden');
+    reviewList[reviewCount].classList.add('visually-hidden');
+    reviewCountText.textContent = reviewCount;
+  }
+};
+
+const clickSliderForward = function (event) {
+  let reviewCount = Number(reviewCountText.textContent);
+  if (reviewCount !== reviewsLength) {
+    reviewCount = reviewCount + 1;
+    reviewList[reviewCount - 1].classList.remove('visually-hidden');
+    reviewList[reviewCount - 2].classList.add('visually-hidden');
+    reviewCountText.textContent = reviewCount;
+  }
 };
